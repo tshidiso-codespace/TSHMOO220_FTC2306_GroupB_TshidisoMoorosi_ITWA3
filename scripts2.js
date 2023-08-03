016ec0617d29ecae478ed4bcb5320b0d3493a263
tshidiso-codespace <tshidisomoorosi@gmail.com>
2023-08-03 21:53:50 +0200

    Initial commit

---
+++ scripts2.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+import { role as nwabisaRole } from './nwabisa.js'
+import { role as johannesRole } from './johannes.js'
+import { role as alexRole } from './alex.js'
+
+console.log('Roles:', nwabisaRole, johannesRole, alexRole)
